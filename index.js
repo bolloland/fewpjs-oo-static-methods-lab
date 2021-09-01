4d6eb072f0586c70c1d7671ac7ad2afdd1c551cc
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-09-01 02:03:54 -0500

    commit tree e4e54effa3650a2d0793d225a30c3f7fce160aab

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+    static capitalize(name) {
+        return name[0].toUpperCase() + name.substring(1)
+    }
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+    }
+    static titleize(string) {
+        let titleArray = []
+        let words = string.split(" ")
+        console.log(words)
+        titleArray.push( this.capitalize(words[0]))
+        let last = words.slice(1)
+        for (let i = 0; i < last.length; i++) {
+            if (last[i] == "the" || last[i] == "a" || last[i] == "an" || last[i] == "but" || last[i] == "of" || last[i] == "and" || last[i] == "for" || last[i] == "at" || last[i] == "by" || last[i] == "from") {
+                titleArray.push(last[i])
+            } else {
+                titleArray.push(this.capitalize(last[i]))
+            }
+            }
+            // word[0].toUpperCase() + word.substring(1)
+          
+            return titleArray.join(" ")
+            console.log(titleArray)
+    }   
+    
 }
